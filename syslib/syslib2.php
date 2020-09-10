ab040bf911df21109f437dce201d5c611390e4c1
danieltiamorim <danieltiamorim@gmail.com>
2020-09-10 00:16:23 -0500

    syslib

---
+++ syslib/syslib2.php
@@ -0,0 +1,30 @@
+<?php
+//syslib_cadastro.php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is coming from a form
+	$titulo = $_POST["titulo"]; //set PHP variables like this so we can use them anywhere in code below
+	$u_autor = $_POST["autor"];	
+	$isbn = $_POST ["isbn"];
+	$preco = $_POST ["preco"];
+	$u_quantidade = $_POST["quantidade"];
+	$editora = $_POST["editora"];
+	$ano = $_POST["ano"];
+	$edicao = $_POST["edicao"];
+	$estado = $_POST["estado"];
+	$custo = $_POST["custo"];
+	$descricao = $_POST["descricao"];
+	
+	
+	//print output text
+	print "Aí sim! Livro cadastrado com sucesso!";
+	print " Clique aqui para cadastrar mais um super livro"; 
+}
+?>
+<html>
+<head> 
+</head>
+<body>
+</br>
+<a href="http://localhost/syslib/Hello%20%20Wolrd.html"> Clique aqui para continuar cadastrando! </a>
+</body>
+
+ </html>
