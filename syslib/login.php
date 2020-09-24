18ac6080996b83bb00fc3e443d8f5dc43c9be108
danieltiamorim <danieltiamorim@gmail.com>
2020-09-24 14:27:41 -0500

    ok

--- syslib/login.php
+++ syslib/login.php
@@ -1,35 +1,45 @@
 <?php
-  if ($_SERVER["REQUEST_METHOD"] == "POST") 
 
-  $host = "localhost";
+
+
+   $host = "localhost";
   $dbusername = "root";
   $dbpassword = "";
   $dbname = 'livraria';
 
 $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
   
- $entrar = $_POST['entrar'];
-  $login = $_POST['login'];
-  $senha=  MD5($_POST['senha']);
-                         
+
  
-     if (isset($entrar)) {
-
-      $verifica = ("SELECT login, senha FROM usuarios WHERE login = '$login' AND senha = '$senha'");
-
-        if (($verifica) == false) {
+ $entrar = ($_POST['entrar']);
+  $login = ($_POST['login']);
+  $senha=  MD5 ($_POST['senha']);
+                         
+ if (empty($login) or empty ($senha)):
+   Echo  "<li> O campo Login / senha precisa ser preenchido</li>";
+ else:
+    $sql = "SELECT login FROM usuarios WHERE login = '$login'";
+    $resultado = mysqli_query ($conn, $sql);
+    if(mysqli_num_rows($resultado) >0):
+      
+     $verifica = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
+     $resultado = mysqli_query ($conn, $verifica );
+     
+        if (mysqli_num_rows($resultado) == 1):
+          setcookie("login",$login);
+          header("Location:home.php");
+        else:
+       
           echo"<script language='javascript' type='text/javascript'>
           alert('Login e/ou senha incorretos');window.location
           .href='login.html';</script>";
-          die();
-        }
-        
-        if (($verifica) == true )  {
-          setcookie("login",$login);
-          header("Location:home.php");
-        }
-        }
+          
+        endif;
+
+    else: 
+        Echo "<li> Usuário e Senha não conferem </li>";
       
-    
-  
+    endif;
+  endif;
+       var_dump  ('$login' and '$senha')
   ?>
